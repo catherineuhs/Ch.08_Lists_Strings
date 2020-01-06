1602b700e256a2127730d6c2db78874ddfd4aa7c
catherineuhs <cliffordcat@urbandaleschools.com>
2020-01-05 19:47:05 -0600

    :^)

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -6,3 +6,30 @@ ADVENTURE PROGRAM
 3.) Expand your program to make it a real adventure game
 
 '''
+
+room_list = []
+
+room = "You are on the front porch", 2, None, None, None
+room_list.append(room)
+room = "You have entered into Bedroom 2", 4, 2, None, None
+room_list.append(room)
+room = "You are on the front South Hall", 5, 3, 0, 1
+room_list.append(room)
+room = "You are in the Dining Room", 6, None, None, 2
+room_list.append(room)
+room = "You are in Bedroom 1", None, 5, 1, None
+room_list.append(room)
+room = "You are in the North Hall", 7, 6, 2, 4
+room_list.append(room)
+room = "You are in the kitchen",  None, None, 3, 5
+room_list.append(room)
+room = "You are on the Balcony", None, None, 5, None
+room_list.append(room)
+
+current_room = 0
+
+done = False
+while not done:
+
+
+
