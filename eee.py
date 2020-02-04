abf3cc6eafa0d02ec29ae56b0527c0db06c30266
catherineuhs <cliffordcat@urbandaleschools.com>
2020-02-04 14:17:59 -0600

    :^)

--- eee.py
+++ eee.py
@@ -180,17 +180,73 @@ def average_list(list):
 
 ####################################
 #JEDI TRAINING
-def increase(x):
-    return x+1
-num = int(input("Enter a number:"))
-num2 = increase(num)
-print("Your number has been increased to", num2)
+# #1)
+# def increase(x):
+#     return x+1
+# num = int(input("Enter a number:"))
+# num2 = increase(num)
+# print("Your number has been increased to", num2)
+#
+
+#2)
+# def count_to_ten():
+#     for i in range(1,11):
+#         print(i)
+# count_to_ten()
+
+#3)
+###SOMETHING WRONG WITH THIS ONE###
+# def sum_list(list):
+#     sum=0
+#     for i in list:
+#         sum+=1
+#     return sum
+# list = [45,2,10,-5,100]
+# print(sum_list(list))
+
+
+
+
+#4)
+
+#####WSOMETHING WRONG LMAO#########
+# def reverse(text):
+#     result = ""
+#     text_length = len(text)
+#     for i in range(text_length):
+#         result = result + text[i*-1-1]
+#     return result
+# text = input("Enter a sentence: ")
+# print(reverse())
+
+
+
+#5)
+# 5.) Correct the following code: (if one of the options is not entered it should print the statements)
+
+
+def get_user_choice():
+    while True:
+        command = input("Command: ")
+    if command == "f" or command == "m" or command == "s" or command == "d" or command == "q":
+        return command
+    else:
+        print("Hey, that's not a command. Here are your options:")
+        print("f - Full speed ahead")
+        print("m - Moderate speed")
+        print("s - Status")
+        print("d - Drink")
+        print("q - Quit")
+
+user_command = get_user_choice()
+
+print("You entered:", user_command)
+
 
 # if __name__ == "__main__":
 #     main()
 #
-hehehe
-
+################################################
 
 
 
